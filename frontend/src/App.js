007d20a237d60dc4116fdff7fe239fb145d35f6d
chuxuanquan <qcx60990@gmail.com>
2022-04-03 17:51:07 -0400

    spinner in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,20 @@ import Search from "./components/Search";
 import ImageCard from "./components/ImageCard";
 import { Container, Row, Col } from "react-bootstrap";
 import Welcome from "./components/Welcome";
-
+import Spinner from "./components/Spinner";
 
 // const API_URL = "https://192.168.1.162:5050"
 
 function App() {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getSavedImages = async () => {
     try {
       const res = await axios.get(`http://127.0.0.1:5050/images`);
       setImages(res.data || []);
+      setLoading(false)
 
     } catch (error) {
       console.log(error);
@@ -50,21 +52,28 @@ function App() {
     setWord("");
   };
 
-  const handleDeleteImage = (id) => {
-    setImages(images.filter((image) => image.id !== id));
+  const handleDeleteImage = async (id) => {
+    try {
+      const res = await axios.delete(`http://127.0.0.1:5050/images/${id}`)
+      if (res.data?.deleted_id) {
+        setImages(images.filter((image) => image.id !== id));
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleSaveImage = async (id) => {
     const imageToBeSaved = images.find((image) => image.id === id);
     imageToBeSaved.saved = true;
-    
+
     try {
       const res = await axios.post(`http://127.0.0.1:5050/images`, imageToBeSaved);
       if (res.data?.inserted_id) {
-        setImages(images.map((image) => image.id === id ? {...image, saved: true} : image));
+        setImages(images.map((image) => image.id === id ? { ...image, saved: true } : image));
       }
       console.log(res.data);
-      
+
     } catch (error) {
       console.log(error);
     }
@@ -73,20 +82,25 @@ function App() {
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      <Container className="mt-4">
-        {images.length ? (
-          <Row xs={1} md={2} lg={3}>
-            {images.map((image, i) => (
-              <Col key={i} className="pb-3">
-                <ImageCard image={image} deleteImage={handleDeleteImage} saveImage={handleSaveImage}/>
-              </Col>
-            ))}
-          </Row>
-        ) : (
-          <Welcome />
-        )}
-      </Container>
+      {loading ? (<Spinner />) : (
+        <>
+
+          <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+          <Container className="mt-4">
+            {images.length ? (
+              <Row xs={1} md={2} lg={3}>
+                {images.map((image, i) => (
+                  <Col key={i} className="pb-3">
+                    <ImageCard image={image} deleteImage={handleDeleteImage} saveImage={handleSaveImage} />
+                  </Col>
+                ))}
+              </Row>
+            ) : (
+              <Welcome />
+            )}
+          </Container>
+        </>
+      )}
     </div>
   );
 }
---
+++ frontend/src/components/Spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Spinner as Loader } from "react-bootstrap";
+
+const spinnerStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%'
+};
+
+const Spinner = () => {
+    return (
+        <Loader style={spinnerStyle} animation="border" variant="primary"/>
+    )
+};
+
+export default Spinner;
